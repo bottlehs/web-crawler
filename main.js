020250808942981a3751f43c29dd22f81ecc8320
bottlehs <qudgns9@gmail.com>
2020-10-28 19:49:54 +0900

    코드 정리

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-const motie = require("./motie.js");
-const msit = require("./msit.js");
-const mss = require("./mss.js");
+const motie = require("./motie.js"); // 산업통상자원부
+const msit = require("./msit.js"); // 과학기술정보통신부
+const mss = require("./mss.js"); // 중소벤처기업부
+const pps = require("./pps.js"); // 조달청
 
 async function handleAsync() {
+  // 산업통상자원부
   const motieRes = await motie.getItems();
   console.log(motieRes);
+  // 과학기술정보통신부
   const msitRes = await msit.getItems();  
   console.log(msitRes);
+  // 중소벤처기업부
   const mssRes = await mss.getItems();  
   console.log(mssRes);  
+  // 조달청
+  const mssPps = await pps.getItems();  
+  console.log(mssPps);    
 }
 
 handleAsync();
--- pps.js
+++ pps.js
@@ -2,68 +2,74 @@
  * 조달청 - 국내지원사업 크롤링
  *
  * @author bottlehs
- * @description 조달청 국내지원사업 크롤링
- *
+ * @description 조달청 - 국내지원사업 크롤링
  */
 
  /**
   * @todo
-  * 1. 링크 접근을 못하도록 되어 있음, java 세션으로 상세보기 관리하기 때문에 상세보기 링크가 존재 하지 않음 공지사항에서 유사 링크는 찾을수 있으므로 알아 볼것
+  */
+
+ /**
+  * @issue
+  * url 존재하지 않음 ( Spring cookie 로 페이지 상세보기 구현됨 )
   */
 
   /**
   * ResultType : HTML
   */
-const axios = require("axios");
-const cheerio = require("cheerio");
-const office  = '조달청';
+ const axios = require("axios");
+ const cheerio = require("cheerio");
+ const office  = '조달청';
+ 
+ async function getHtml() {
+   try {
+     return await axios.get("https://www.pps.go.kr/gpass/domestic/listDomestic.do");
+   } catch (error) {
+     console.error(error);
+   }
+ }
+ 
+ async function getItems() {
+   let items = [];
+   const response = await getHtml();
+   const $ = cheerio.load(response.data);
 
-const getHtml = async () => {
-  try {
-    return await axios.get("https://www.pps.go.kr/gpass/domestic/listDomestic.do");
-  } catch (error) {
-    console.error(error);
-  }
-};
+   $("table.tstyle_list tbody").children("tr").each(function(i, element) {
+      const item = {
+        type: '', // 구분
+        title: '', // 공고명
+        period: '', // 신청기간
+        people: '', // 신청인원/모집인원
+        state: '', // 진행상태
+        url: '', // 링크    
+        office: office // 소관부처      
+      };
 
-getHtml()
-  .then(response => {
-    let urlList = [];
-    const $ = cheerio.load(response.data);
-    const $bodyList = $("table.tstyle_list tbody").children("tr");
-
-    let type = ''; // 구분
-    let title = ''; // 공고명
-    let period = ''; // 신청기간
-    let people = ''; // 신청인원/모집인원
-    let state = ''; // 진행상태
-    let url = ''; // 링크
-    $bodyList.each(function(i, element) {
       $(this).find('td').each(function(j, element) {
         if ( element.children[0].data ) {
           if ( j == 1 ) {
             // 구분
-            type = element.children[0].data.trim();
+            item.type = element.children[0].data.trim();
           };
 
           if ( j == 3 ) {
             // 기간
-            period = element.children[0].data.trim() +' '+ element.children[2].data.trim();            
+            item.period = element.children[0].data.trim() +' '+ element.children[2].data.trim();            
           };
 
           if ( j == 4 ) {
             // 신청인원/모집인원
-            people = element.children[0].data.trim();                        
+            item.people = element.children[0].data.trim();                        
           };          
 
           if ( j == 5 ) {
             // 진행상태
-            state = element.children[0].data.trim();                                    
+            item.state = element.children[0].data.trim();                                    
           };          
         };
       });
 
-      title = $(this).find('td a').text().trim();
+      item.title = $(this).find('td a').text().trim();
 
       // 링크 ( pre-link )
       let temp = $(this).find('td a').attr('href');     
@@ -71,20 +77,11 @@ getHtml()
       var reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi
       temp = temp.replace("javascript:fn_detailView","");
       temp = temp.replace(reg, "");
-      console.log(temp);
 
-      urlList[i] = {
-        type: type,    
-        title: title,
-        period: period,
-        people: people,
-        state: state,
-        url: url,
-        office : office
-      };      
+      items.push(item);
     });
 
-    const data = urlList.filter(n => n.title);
-    return data;
-  })
-  .then(res => console.log(res));
+    return items;
+ };
+ 
+ module.exports = { getItems };
