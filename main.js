09ecbaef98785e7491b1d240e934caf6f1216812
bottlehs <qudgns9@gmail.com>
2020-10-28 16:24:05 +0900

    중소기업벤처부 - 사업공고 크롤링

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const motie = require("./motie.js");
 const msit = require("./msit.js");
+const mss = require("./mss.js");
 
 async function handleAsync() {
   const motieRes = await motie.getItems();
   console.log(motieRes);
   const msitRes = await msit.getItems();  
   console.log(msitRes);
+  const mssRes = await mss.getItems();  
+  console.log(mssRes);  
 }
 
 handleAsync();
--- mss.js
+++ mss.js
@@ -2,70 +2,70 @@
  * 중소기업벤처부 - 사업공고 크롤링
  *
  * @author bottlehs
- * @description 중소기업벤처부 사업공고 크롤링
+ * @description 중소기업벤처부 - 사업공고 크롤링
  *
  */
+
+ /**
+  * @todo
+  */
 
   /**
   * ResultType : HTML
   */
-const axios = require("axios");
-const cheerio = require("cheerio");
-const office  = '중소기업벤처부';
+ const axios = require("axios");
+ const cheerio = require("cheerio");
+ const office  = '중소기업벤처부';
+ 
+ async function getHtml() {
+   try {
+     return await axios.get("https://www.mss.go.kr/site/smba/ex/bbs/List.do?cbIdx=310");
+   } catch (error) {
+     console.error(error);
+   }
+ }
+ 
+ async function getItems() {
+   let items = [];
+   const response = await getHtml();
+   const $ = cheerio.load(response.data);
 
-const getHtml = async () => {
-  try {
-    return await axios.get("https://www.mss.go.kr/site/smba/ex/bbs/List.do?cbIdx=310");
-  } catch (error) {
-    console.error(error);
-  }
-};
+   $(".board_list.type_notice table tbody").children("tr.notice").each(function(i, element) {
 
-getHtml()
-  .then(response => {
-    let urlList = [];
-    const $ = cheerio.load(response.data);
-    const $bodyList = $(".board_list.type_notice table tbody").children("tr.notice");
+    const item = {
+      title: "", // 제목
+      url: "",  // 링크
+      date: "",  // 날짜
+      department: "",  // 부서
+      office: office // 소관부처
+    };
 
-    $bodyList.each(function(i, element) {
-      // https://www.mss.go.kr/site/smba/ex/bbs/View.do?cbIdx=310&bcIdx=1022229&parentSeq=1022229
+    $(this).find('td.mobile div.info ul li span').each(function(j, element) {
+      if ( j == 1 ) {
+        item.department = element.children[0].data;
+      };
+      if ( j == 3 ) {
+        item.date = element.children[0].data;
+      };       
+    });    
 
-      // 담당부서, 등록일 추출
-      let date = '';
-      let department = '';
-      let url = '';
-      $(this).find('td.mobile div.info ul li span').each(function(j, element) {
-        if ( j == 1 ) {
-          department = element.children[0].data;
-        };
-        if ( j == 3 ) {
-          date = element.children[0].data;
-        };       
-      });
-
-      // URL 추출
-      let temp = $(this).find('td.mobile a').attr('onclick');
-      var reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi
-      temp = temp.replace("doBbsFView('","");
-      temp = temp.replace("');return false;","");
-      let tempArray = temp.split(',');
-      tempArray.forEach((item, j) => {
-        item = item.replace(reg, "");
-        tempArray[j] = item;
-      });
-
-      url = 'https://www.mss.go.kr/site/smba/ex/bbs/View.do?cbIdx='+tempArray[0]+'&bcIdx='+tempArray[1]+'&parentSeq='+tempArray[3];
-
-      urlList[i] = {
-        title: $(this).find('td.mobile div.subject strong').text(),
-        url: url,
-        date: date,
-        department: department,
-        office : office
-      };      
+    // URL 추출
+    let temp = $(this).find('td.mobile a').attr('onclick');
+    var reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi
+    temp = temp.replace("doBbsFView('","");
+    temp = temp.replace("');return false;","");
+    let tempArray = temp.split(',');
+    tempArray.forEach((item, j) => {
+      item = item.replace(reg, "");
+      tempArray[j] = item;
     });
 
-    const data = urlList.filter(n => n.title);
-    return data;
-  })
-  .then(res => console.log(res));
+    item.url = 'https://www.mss.go.kr/site/smba/ex/bbs/View.do?cbIdx='+tempArray[0]+'&bcIdx='+tempArray[1]+'&parentSeq='+tempArray[3];
+    item.title = $(this).find('td.mobile div.subject strong').text(),
+    items.push(item);
+  });   
+
+  return items;
+ };
+ 
+ module.exports = { getItems };
