16bce8e14ef0ca2bd49497a338bb0144f5675d2e
bottlehs <qudgns9@gmail.com>
2020-10-30 00:09:39 +0900

    no message

--- main.js
+++ main.js
@@ -2,12 +2,9 @@ const motie = require("./motie.js"); // 산업통상자원부
 const msit = require("./msit.js"); // 과학기술정보통신부
 const mss = require("./mss.js"); // 중소벤처기업부
 const pps = require("./pps.js"); // 조달청
-<<<<<<< HEAD
 const kimst = require("./kimst.js"); // 해양수산부
 const mfds = require("./mfds.js"); // 식품의약품안전처
 const me = require("./me.js"); // 환경부
-=======
->>>>>>> parent of 7f82e9a... 식품의약품안전처 - 사업공고 크롤링
 
 async function handleAsync() {
   // 산업통상자원부
@@ -22,7 +19,6 @@ async function handleAsync() {
   // 조달청
   const mssPps = await pps.getItems();  
   console.log(mssPps);    
-<<<<<<< HEAD
   // 해양수산부
   const kimstPps = await kimst.getItems();  
   console.log(kimstPps);      
@@ -32,8 +28,6 @@ async function handleAsync() {
   // 환경부
   const mePps = await me.getItems();  
   console.log(mePps);        
-=======
->>>>>>> parent of 7f82e9a... 식품의약품안전처 - 사업공고 크롤링
 }
 
 handleAsync();
