5fa8e98fd93cbe2025913f85d111d67b11bd42f0
bottlehs <qudgns9@gmail.com>
2020-11-15 04:31:59 +0900

    티빙

---
+++ ott/tving_video.js
@@ -0,0 +1,82 @@
+/**
+ * 티빙 - 크롤링
+ *
+ * @author bottlehs
+ * @description 티빙 - 크롤링
+ *
+ */
+
+ /**
+  * @todo
+  */
+
+ /**
+  * @issue
+  */  
+
+  /**
+  * ResultType : Json
+  */
+ const axios = require("axios");
+ const office  = '티빙';
+ const fs = require('fs');
+
+ let page = 1;
+ let isLast = true;
+
+ async function getHtml() {
+   try {
+    const params = {
+      'pageNo':page,
+      'pageSize':24,
+      'order':'viewDay',
+      'adult':'all',
+      'free':'all',
+      'guest':'all',
+      'scope':'all',
+      'lastFrequency':'y',
+      'personal':'N',
+      'screenCode':'CSSD0100',
+      'networkCode':'CSND0900',
+      'osCode':'CSOD0900',
+      'teleCode':'CSCD0900',
+      'apiKey':'1e7952d0917d6aab1f0293a063697610'
+    }
+
+    return await axios.get("https://api.tving.com/v2/media/episodes", {
+      params: params,
+    });
+   } catch (error) {
+     console.error(error);
+   }
+ }
+ 
+ async function getItems() {
+    let items = [];
+    while (isLast) {
+      const response = await getHtml();
+      const data = response.data;
+      if ( data.body.result ) {     
+        console.log(page);   
+        const fileName = 'tving/tving_video_'+page + '.json';
+        fs.writeFileSync(fileName, JSON.stringify(data.body.result));    
+      } else {
+        isLast = false;        
+      }
+
+      page++;
+    }
+
+    return items;
+ };
+ 
+ module.exports = { getItems };
+
+ // 개별실행
+ async function handleAsync() {
+  // 티빙
+  const wavveRes = await getItems();
+  console.log(wavveRes);
+}
+
+handleAsync();
