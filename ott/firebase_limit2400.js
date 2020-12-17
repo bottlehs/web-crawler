685b0a537e895780789689d7dcfd6646da2e792e
bottlehs <qudgns9@gmail.com>
2020-12-17 09:02:21 +0900

    수정

--- ott/firebase_limit2400.js
+++ ott/firebase_limit2400.js
@@ -135,7 +135,7 @@ async function handleAsync() {
   let complate = 0;
   for(let i = 0; i < json.length; i++) {
     let file_uuid = uuid();
-    if ( 9996 <= i ) {
+    if ( 13552 <= i ) {
       // console.log(json[i]);
       // console.log("번호 : "+i)sss
 
